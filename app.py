58922927e5c3342f9f2e9f4127e4a7a3af007843
Muhammad <mhk8367@mavs.uta.edu>
2024-05-15 01:44:33 -0500

    Vercel Readjustments

--- app.py
+++ app.py
@@ -16,19 +16,6 @@ def index():
 
 @app.route('/', methods=['POST'])
 def predict():
-    imagefile= request.files['imagefile']
-    image_path = "./images/" + imagefile.filename
-    imagefile.save(image_path)
-
-    image = load_img(image_path, target_size=(224, 224))
-    image = img_to_array(image)
-    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
-    image = preprocess_input(image)
-    yhat = model.predict(image)
-    label = decode_predictions(yhat)
-    label = label[0][0]
-
-    classification = '%s (%.2f%%)' % (label[1], label[2]*100)
     return render_template('index.html')
 
 @app.route('/video')
